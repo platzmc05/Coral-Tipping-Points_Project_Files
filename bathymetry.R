0fd53afae19eb0f46263419b959dacfb59b9a5bb
meganrk1 <megankramer@usf.edu>
2021-07-21 19:51:23 +0000

    Added libraries to bathymetry plots

--- bathymetry.R
+++ bathymetry.R
@@ -2,8 +2,10 @@
 
 #DEM
 
+library(ggplot2)
+
 library(stars)
-bath <- read_stars("../Coral Tipping Points/SESYNC_Project_Files/FRT_CRM.tiff", proxy = TRUE)
+bath <- read_stars("FRT_CRM.tiff", proxy = TRUE)
 
 st_crs(bath)
 st_crs
@@ -17,18 +19,19 @@ ggplot() +
 #CREMP Reef locations to overlay
 
 library(sf)
-rdata <- fread('../Coral Tipping Points/SESYNC_Project_Files/Station_List_Master_200712.csv')
+library(data.table)
+rdata <- fread('Station_List_Master_200712.csv')
 
 reefs <- st_as_sf(rdata, coords = c("lonDD", "latDD"), 
                   crs = st_crs(bath)
                   )
-# Combined bathymetry + reef locations 
+#Plot of combined bathymetry + reef locations 
   
 ggplot() +
   geom_stars(data = bath) +
   geom_sf(data = reefs, size = 0.5, color = 'red')
 
-## Mapview
+#Mapview of combined bathymetry + reef locations
 
 library(mapview)
 mapview(bath)
@@ -37,5 +40,9 @@ mapview(bath, legend = FALSE, alpha = 0.3,
         maxpixels = 4891525, map.types = 'Esri.WorldImagery' ) +
   mapview(reefs, legend = FALSE)
 
+
+#Storage
+#../Coral Tipping Points/SESYNC_Project_Files/
+#../Coral Tipping Points/SESYNC_Project_Files/
 
 
